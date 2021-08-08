c47ad016fd1009627a50c6b33ff1e6815105f885
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-08-08 15:25:39 +0200

    add margin to elements

--- src/components/MapCard.js
+++ src/components/MapCard.js
@@ -5,73 +5,78 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import ReactStars from "react-rating-stars-component";
+import ReactStars from 'react-rating-stars-component';
 const useStyles = makeStyles({
-    root: {
-      width: 280,
-      maxHeight: 300
-    },
-    media: {
-      height: 100
-    },
-  });
+  root: {
+    width: 280,
+    maxHeight: 300,
+  },
+  media: {
+    height: 100,
+  },
+});
 
-function MapCard(props){
-    const classes = useStyles();
-    
-    return(
-        
-            <Card className={classes.root}>
-            <CardActionArea>
-                <CardMedia
-                className={classes.media}
-                image={props.img}
-                title="Contemplative Reptile"
+function MapCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.img}
+          title='Contemplative Reptile'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h6' component='h6'>
+            <div className='Card-map--name'>{props.name}</div>
+          </Typography>
+
+          <Typography variant='body2' color='textSecondary' component='p'>
+            <div className='rating-container'>
+              <span className='Card-map--rating'>{props.rating + ' '}</span>
+
+              <span className='Card-map--stars'>
+                <ReactStars
+                  className='Card-map--stars'
+                  value={props.rating}
+                  edit={false}
+                  isHalf={true}
                 />
-                <CardContent>
-                    <Typography gutterBottom variant="h6" component="h6">
-                        <div className="Card-map--name">
-                            {props.name}
-                        </div>
-                    </Typography>
-                    
-                        
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            <div className="rating-container">
-                            <span className="Card-map--rating">{props.rating+" "}</span>
-                            
-                                <span className="Card-map--stars">
-                                <ReactStars 
-                                    className="Card-map--stars"
-                                    
-                                    value={props.rating}
-                                    edit={false}
-                                    isHalf={true}
-                                /> 
-                            </span> 
-                            
-                                <span className="Card-map--raters"> ({props.usrs})</span>
-                            
-                            </div>
-                            </Typography>
-                       
-                        <Typography gutterBottom variant="body2" color="textSecondary" component="p">
-                        <div className="Card-map--type">{props.cuisine} {props.type}</div>
-                        </Typography>
-                        <div className="Card-map--hours"> 
-                            {props.open ?
-                            <li className="Card-map--hours open">Open until {props.closeHour}</li>
-                            :
-                            <div>
-                            <li className="Card-map--hours card-res--name close" >Closed</li>
-                            <li className="Card-map--hours open">Openes at {props.openHour}</li>
-                            </div>}
-                        </div>
-                </CardContent>
-                </CardActionArea>
-            </Card>
-        
-    );
+              </span>
+
+              <span className='Card-map--raters'> ({props.usrs})</span>
+            </div>
+          </Typography>
+
+          <Typography
+            gutterBottom
+            variant='body2'
+            color='textSecondary'
+            component='p'
+          >
+            <div className='Card-map--type'>
+              {props.cuisine} {props.type}
+            </div>
+          </Typography>
+          <div className='Card-map--hours'>
+            {props.open ? (
+              <li className='Card-map--hours open'>
+                Open until {props.closeHour}
+              </li>
+            ) : (
+              <div>
+                <li className='Card-map--hours card-res--name close'>Closed</li>
+                <li className='Card-map--hours open'>
+                  Openes at {props.openHour}
+                </li>
+              </div>
+            )}
+          </div>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
 
 export default MapCard;
